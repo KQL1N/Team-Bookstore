4cfae2a8ae458a804db62f0bfa44e25eeec0c8ad
hcevans <hannah.c.evans@live.co.uk>
2017-01-27 15:09:36 +0000

    Book Entity

---
+++ Bookstore/src/main/java/com/qa/persistence/Bookstore/Book.java
@@ -0,0 +1,17 @@
+package com.qa.persistence.Bookstore;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//import javax.persistence.EntityManager;
+//import javax.persistence.EntityManagerFactory;
+//import javax.persistence.EntityTransaction;
+//import javax.persistence.Persistence;
+
+//import com.qa.persistence.CD;
+//import com.qa.persistence.Musician;
+
+//@Entity
+public class Book {
+
+}
