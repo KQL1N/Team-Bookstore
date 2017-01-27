a0c42382f094ef12582b0454f852f82c30ac7883
MarkBeresford <mberesford301@gmail.com>
2017-01-27 15:21:21 +0000

    "Book Entity"

--- Bookstore/src/main/java/com/qa/persistence/Bookstore/Book.java
+++ Bookstore/src/main/java/com/qa/persistence/Bookstore/Book.java
@@ -6,7 +6,7 @@ import java.util.Set;
 //import javax.persistence.EntityManager;
 //import javax.persistence.EntityManagerFactory;
 //import javax.persistence.EntityTransaction;
-//import javax.persistence.Persistence;
+// import javax.persistence.Persistence;
 
 //import com.qa.persistence.CD;
 //import com.qa.persistence.Musician;
